832260a45029056cf48b3850f90f38d2330fd656
marbenz2 <benzinger.marcel@gmail.com>
2024-08-05 16:59:19 +0200

    style: Update Social component gap to 4 for better spacing

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -2,7 +2,7 @@ const Social = () => {
   return (
     <div className="flex flex-col gap-2">
       <h2 className="text-lg md:text-2xl font-semibold ">Socialmedia</h2>
-      <div className="flex gap-2">
+      <div className="flex gap-4">
         <a
           href="https://github.com/marbenz2/"
           target="_blank"
