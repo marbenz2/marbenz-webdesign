18d1de96646027e550aad95da922871797a45927
marbenz2 <benzinger.marcel@gmail.com>
2024-08-05 17:00:12 +0200

    feat: Add about component for displaying personal information

---
+++ src/data/about.tsx
@@ -0,0 +1,12 @@
+export const about = (
+  <>
+    <span className="text-4xl lg:text-6xl font-normal">Hey!</span>
+    <>
+      Ich bin Marcel. Als Junior Webdesigner und Webentwickler bringe ich
+      frischen Wind und Kreativität in jedes Projekt. Mit meinem technischen
+      Hintergrund und meiner Leidenschaft für ansprechendes Design freue ich
+      mich darauf, meine Fähigkeiten einzubringen und gleichzeitig Neues zu
+      lernen, um gemeinsam großartige digitale Lösungen zu entwickeln!
+    </>
+  </>
+);
