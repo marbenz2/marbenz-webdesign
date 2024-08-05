8cd36cd2cd615e0a4d247e878ef72ca58e181759
marbenz2 <benzinger.marcel@gmail.com>
2024-08-05 17:08:07 +0200

    refactor: Update About component to use h2 tag for better semantic structure

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,9 +6,9 @@ const About = () => {
       <span className="text-contrast/40 font-mono absolute -top-8 lg:-top-16 -left-8 lg:-left-16 text-9xl lg:text-[256px]">
         &ldquo;
       </span>
-      <p className="relative text-start text-muted-foreground text-lg lg:text-2xl font-thin w-full self-center leading-relaxed">
+      <h2 className="relative text-start text-muted-foreground text-lg lg:text-2xl font-thin w-full self-center leading-relaxed">
         {about}
-      </p>
+      </h2>
     </div>
   );
 };
