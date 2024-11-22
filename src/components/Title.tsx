cdc2dbd5b61a6056718ac660e433c27ac309d78c
marbenz2 <benzinger.marcel@gmail.com>
2024-11-22 19:59:44 +0100

    fix: removed unnecessary z-index from Title component

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,14 +4,14 @@ const Title = () => {
   return (
     <div className="flex flex-col w-fit self-center lg:self-start">
       <div className="flex flex-col md:flex-row md:gap-4 lg:flex-col lg:gap-0 2xl:flex-row 2xl:gap-4">
-        <h1 className="text-6xl lg:text-7xl lg:text-start text-contrast z-50">
+        <h1 className="text-6xl lg:text-7xl lg:text-start text-contrast">
           Marcel
         </h1>
-        <h1 className="text-6xl lg:text-7xl lg:text-start text-contrast z-50">
+        <h1 className="text-6xl lg:text-7xl lg:text-start text-contrast">
           Benzinger
         </h1>
       </div>
-      <div className="flex pl-1 lg:pl-2 -mt-3 md:-mt-3 lg:-mt-4 self-start z-40">
+      <div className="flex pl-1 lg:pl-2 -mt-3 md:-mt-3 lg:-mt-4 self-start">
         <TypeAnimation
           sequence={[
             "",
